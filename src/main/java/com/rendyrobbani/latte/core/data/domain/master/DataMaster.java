ee1f568c7715a83f1420b2cabba4b754f54fc344
Rendy Robbani <rendyrobbani17@gmail.com>
2025-08-05 19:11:21 +0800

    Espresso

--- src/main/java/com/rendyrobbani/latte/core/data/domain/master/DataMaster.java
+++ src/main/java/com/rendyrobbani/latte/core/data/domain/master/DataMaster.java
@@ -1,7 +1,6 @@
 package com.rendyrobbani.latte.core.data.domain.master;
 
 import com.rendyrobbani.latte.core.data.domain.Lockable;
-import com.rendyrobbani.latte.core.data.domain.user.DataUser;
 
 public interface DataMaster<ID> extends Lockable {
 
